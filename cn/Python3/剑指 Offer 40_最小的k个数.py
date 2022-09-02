32f6d81d313ebb348696bcb2127c964af546d6e1
chaolei.fu <1310866178@qq.com>
2022-09-02 19:35:40 +0800

    git

---
+++ cn/Python3/剑指 Offer 40_最小的k个数.py
@@ -0,0 +1,10 @@
+# @algorithm @lc id=100301 lang=python3 
+# @title zui-xiao-de-kge-shu-lcof
+
+class Solution:
+    def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
+        
+# class Solution:
+#     def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
+#         arr.sort()
+#         return arr[:k]

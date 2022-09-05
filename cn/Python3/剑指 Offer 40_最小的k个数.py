9bbb5bd27c84493fc78672077b428b69462f0245
chaolei.fu <1310866178@qq.com>
2022-09-05 11:41:10 +0800

    40

--- cn/Python3/剑指 Offer 40_最小的k个数.py
+++ cn/Python3/剑指 Offer 40_最小的k个数.py
@@ -3,9 +3,25 @@
 
 
 class Solution:
-    def quick(self,l,r)
     def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
+        if len(arr) == k:
+            return arr
 
+        def quicks(arr, l, r, k):
+            i, j = l, r
+            while i < j:
+                while i < j and arr[j] >= arr[l]:
+                    j = j-1
+                while i < j and arr[i] <= arr[l]:
+                    i = i + 1
+                arr[i], arr[j] = arr[j], arr[i]
+            arr[i], arr[l] = arr[l], arr[i]
+            if k < i:
+                return quicks(arr, l, i-1, k)
+            if k > i:
+                return quicks(arr, i+1, r, k)
+            return arr[:k]
+        return quicks(arr, 0, len(arr)-1, k)
 # class Solution:
 #     def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
 #         if k==0:

fdaf1824bee601e831257004fcd96d54571195ac
fuchaolei <chaolei.fu@kuwo.cn>
2022-09-10 21:21:48 +0800

    52

---
+++ cn/Go/剑指 Offer 52.两个链表的第一个公共节点/solution.go
@@ -0,0 +1,28 @@
+// @algorithm @lc id=100326 lang=golang
+// @title liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof
+
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	a, b := headA, headB
+	for a != b {
+		if a != nil {
+			a = a.Next
+		} else {
+			a = headB
+		}
+		if b != nil {
+			b = b.Next
+		} else {
+			b = headA
+		}
+	}
+	return a
+}

5a4fe42c5349f87c15dd1ad2e93acc878af14f46
fuchaolei <chaolei.fu@kuwo.cn>
2022-09-11 21:22:42 +0800

    61

---
+++ cn/C++/question/___Offer_61__________LCOF.cpp
@@ -0,0 +1,16 @@
+// @algorithm @lc id=100341 lang=cpp
+// @title bu-ke-pai-zhong-de-shun-zi-lcof
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "algm/algm.h"
+using namespace std;
+class Solution
+// @test([0,0,1,2,5])=true
+{
+public:
+    bool isStraight(vector<int> &nums)
+    {
+    }
+};

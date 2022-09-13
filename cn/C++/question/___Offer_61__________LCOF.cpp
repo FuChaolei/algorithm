da02884e136f0f88b313deba194290d349b2d7be
fuchaolei <chaolei.fu@kuwo.cn>
2022-09-13 09:01:41 +0800

    61

--- cn/C++/question/___Offer_61__________LCOF.cpp
+++ cn/C++/question/___Offer_61__________LCOF.cpp
@@ -8,9 +8,27 @@
 using namespace std;
 class Solution
 // @test([0,0,1,2,5])=true
+// @test([1,2,3,4,5])=true
 {
 public:
     bool isStraight(vector<int> &nums)
     {
+        int ma=0, mi=14;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 0)
+            {
+                continue;
+            }
+            if (mp[nums[i]] != 0)
+            {
+                return false;
+            }
+            ma = max(ma, nums[i]);
+            mi = min(mi, nums[i]);
+            mp[nums[i]]++;
+        }
+        return ma - mi < 5;
     }
 };

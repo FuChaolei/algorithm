08333ecab7e1e4149aa84b3645f4bfbe413ac128
chaolei.fu <1310866178@qq.com>
2022-09-14 10:35:45 +0800

    39

---
+++ cn/C++/question/___Offer_39_________________LCOF.cpp
@@ -0,0 +1,27 @@
+// @algorithm @lc id=100310 lang=cpp 
+// @title shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include "algm/algm.h"
+using namespace std;
+class Solution {
+// @test([3,3,4])=3
+public:
+    int majorityElement(vector<int>& nums) {
+        int v = 0;
+        int n;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if(v==0){
+                n = nums[i];
+            }
+            else{
+                v+=nums[i]==n?1:-1;
+            }
+        }
+        return n;
+    }
+};
